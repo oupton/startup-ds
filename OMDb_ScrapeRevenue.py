da02f89c268be8f9ce66585d5deab66cefbd9c36
oupton <upton.oliver@gmail.com>
2018-04-26 03:35:25 -0500

    Debugged

--- OMDb_ScrapeRevenue.py
+++ OMDb_ScrapeRevenue.py
@@ -4,7 +4,7 @@ import requests
 import os
 import re
 
-url = ''
+url = 'http://www.omdbapi.com/?apikey={}&i={}&plot=full'
 api_keys = ['6ba7c3ee','df702266', 'bbc27ce', 'a89c2406']
 api_idx = 0
 df = pd.read_csv(os.path.join('data', 'missing_financial_data.csv'))
@@ -14,13 +14,12 @@ budg_only = df[df['budget'] > 0]
 revenue = []
 
 for imdb_id in budg_only['recovered_ids']:
-    omdb_resp = requests.get(url.format(api_keys[api_idx], imdb_id))
-    if omdb_resp != 200 or (omdb_resp.json()['Response'] == u'False' and u'IMDb' not in omdb_resp.json()['Error']):
-        api_idx += 1
-        omdb_resp = requests.get(url.format(api_keys[api_idx], imdb_id))
-
     try:
-        revenue.append(re.replace(r'[^0-9]', '', omdb_resp.json()['BoxOffice']))
+        omdb_resp = requests.get(url.format(api_keys[api_idx], imdb_id))
+        if omdb_resp.status_code != 200 or (omdb_resp.json()['Response'] == u'False' and u'IMDb' not in omdb_resp.json()['Error']):
+            api_idx += 1
+            omdb_resp = requests.get(url.format(api_keys[api_idx], imdb_id))
+        revenue.append(re.sub(r'[^0-9]', '', omdb_resp.json()['BoxOffice']) if len(re.sub(r'[^0-9]', '', omdb_resp.json()['BoxOffice'])) != 0 else '0')
     except:
         revenue.append('0')
 
